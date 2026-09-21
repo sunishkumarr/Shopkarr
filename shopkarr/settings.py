92881fd0f73fbb4ce0e2e44a95d262b874fad917
Sunish Kumar <sunishkumarr@gmail.com>
2026-09-21 11:06:41 +0000

    Product and Category model creation

--- shopkarr/settings.py
+++ shopkarr/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'products'
 ]
 
 MIDDLEWARE = [
