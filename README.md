5c61829794c4236059c0811b6f57cdb64f26b6ea
Sunish Kumar <sunishkumarr@gmail.com>
2026-09-22 13:13:46 +0000

    Implement responsive storefront UI with Tailwind CSS

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Shopkarr
 A E-commerce project
+## Development
+To edit the styling, download the Tailwind v4 Standalone CLI for your OS, place it in the root directory as `tailwindcss`, and run:
+`./tailwindcss -i ./static/src/input.css -o ./static/css/output.css --watch`
--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
+from .models import Category, Product
 
 # Create your views here.
+def product_list(request, category_id=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    if category_id:
+        category = get_object_or_404(Category, id=category_id)
+        products = products.filter(category=category)
+
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products
+    }
+
+    return render(request, 'products/list.html', context)
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/detail.html', context)
